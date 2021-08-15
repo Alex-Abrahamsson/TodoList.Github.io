882c7a3b69f92741a535ede94779564c651bf61e
Alexander Abrahamsson <alexander.abrahams2@learnet.se>
2021-08-15 20:56:02 +0200

    sdsd

--- Main.js
+++ Main.js
@@ -1,9 +1,29 @@
 const inputVal = document.getElementsByClassName("inputVal")[0];
 const addTaskBtn = document.getElementsByClassName("btn")[0];
 
+
+//Funktion för att ta bort 1 "Att göra rad" i taget.
+function deleteItem(index)
+{
+    let localItems = JSON.parse(localStorage.getItem("localItem"))
+    tasklist.splice(index, 1)
+    localStorage.setItem("localItem", JSON.stringify(tasklist))
+    ShowList();
+}
+
+//Funktion för att rensa hela lista.
+function ClearTasks()
+{
+    localStorage.clear();
+    ShowList();
+    inputVal.value = "";
+}
+
+
+//Denna funktion tar hand om "Add task" knappen.
 addTaskBtn.addEventListener("click", () => 
 {
-    if (inputVal.value.trim() != 0)
+    if (inputVal.value.trim() != 0) //Kollar så det är Minst 1 bokstav i textrutan eftersom de är onödigt att lägga till tomma Tasks
     {
         let localItems = JSON.parse(localStorage.getItem("localItem"))
         if (localItems === null)
@@ -36,12 +56,11 @@ function ShowList()
     }
     tasklist.forEach((data, index) => 
     {
-        outPut += `
-        <div class="todoList">
-                    <p class="pText">${data}</p>
-                    <button class="deleteTask" onClick="deleteItem(${index})">X</button>
-                    </div>
-        `
+        outPut += 
+       `<div class="todoList">
+            <p class="pText">${data}</p>
+            <button class="deleteTask" onClick="deleteItem(${index})">X</button>
+        </div>`
     });
     
     taskListShow.innerHTML = outPut;
@@ -50,17 +69,3 @@ function ShowList()
 
 ShowList();
 
-function deleteItem(index)
-{
-    let localItems = JSON.parse(localStorage.getItem("localItem"))
-    tasklist.splice(index, 1)
-    localStorage.setItem("localItem", JSON.stringify(tasklist))
-    ShowList();
-}
-
-function ClearTasks()
-{
-    localStorage.clear();
-    ShowList();
-    inputVal.value = "";
-}
